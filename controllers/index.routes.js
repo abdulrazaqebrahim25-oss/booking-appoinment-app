e4f16e51a92eec41304704293db62ebbb5618622
abdulrazaqebrahim25-oss <abdulrazaqebrahim25@gmail.com>
2026-01-26 19:10:52 +0300

    fix the error

--- controllers/index.routes.js
+++ controllers/index.routes.js
@@ -3,10 +3,11 @@ const Appointment = require('../models/Appointment')
 const Service = require('../models/Service')
 const User = require('../models/User')
 
-router.get('/',async(req,res)=>{
-    const appointment = await Appointment.find()
-    const user = await User.find()
+router.get('/', async (req, res) => {
+    console.log('SESSION USER 👉', req.session.user)
 
-    res.render('homepage.ejs',{appointment, user})
+    const appointments = await Appointment.find()
+
+    res.render('homepage.ejs', {appointments})
 })
 module.exports = router;
